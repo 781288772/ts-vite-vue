5e56909746c7d10b9cebc32bf082e366132a3257
这该死的魅力 <781288772@qq.com>
2021-10-22 16:54:58 +0800

    watchEffect

--- src/components/demo5.vue
+++ src/components/demo5.vue
@@ -4,9 +4,8 @@
  <button @click="increment">add</button> -->
  <div>
    <h1>msg</h1>
-   <h1>{{num}}</h1>
-   <!-- <p>{{state.count}}</p> -->
-   <!-- <input type="text" ref="root" value="ref使用例子"> -->
+   <p>{{state.count}}</p>
+   <input type="text" ref="root" value="ref使用例子">
    <!-- {{count}}
    <h1>{{obj.dd}}</h1>
    <h6>toRefs解构reactive:{{dd}}</h6>
@@ -16,44 +15,21 @@
 
 
 <script setup lang="ts">
-/**
- * 1.不需要手动传入以来
- * 2.不是lazy初始化执行分析依赖
- * 3.无法获取原始值
- * 4.一些异步操作放里面更加合适
- * 5.watch第三个参数处理副作用的第一个参数
- */
-import { watch,ref,reactive,computed,defineProps, toRefs,onMounted,watchEffect} from 'vue'
-const num = ref(0);
+import { watch,ref,reactive,computed,defineProps, toRefs,onMounted } from 'vue'
 defineProps({
   msg:String
 })
-onMounted(()=>{
-  console.log("onMounted")
-});
-const stop =  watchEffect((onInvalidate)=>{
-  console.log("watchEffed之前调用",num.value);
-  onInvalidate(()=>{
-    // 清除副作用（某些元素等）
-
-  })
+const state = reactive({count:0});
+watch(()=>state.count,(count,prevCount)=>{
+  console.log("第一个监听的值",count,prevCount);
+},{
+  deep:true,
+  immediate:true
 })
-setTimeout(() => {
-  num.value++;
-}, 1000);
-// 停用监听
-stop()
-// const state = reactive({count:0});
-// watch(()=>state.count,(count,prevCount)=>{
-//   console.log("第一个监听的值",count,prevCount);
-// },{
-//   deep:true,
-//   immediate:true
-// })
 
-// setTimeout(() => {
-//   state.count++;
-// }, 1000);
+setTimeout(() => {
+  state.count++;
+}, 1000);
 // const count = ref(1);
 // console.log(count.value);
 // // 返回一个readonly
