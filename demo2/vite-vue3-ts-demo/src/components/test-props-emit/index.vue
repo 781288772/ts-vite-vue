75cd0bbaa3d0de8164aae77e7a3472aa6cc4b64b
chy <781288772@qq.com>
2021-11-23 21:31:08 +0800

    define value in setup

--- demo2/vite-vue3-ts-demo/src/components/test-props-emit/index.vue
+++ demo2/vite-vue3-ts-demo/src/components/test-props-emit/index.vue
@@ -1,14 +1,35 @@
 <template>
 <p>{{props.msg}}</p>
 <button @click="handleClick">click me to call the parent commonent's method</button>
+<p>{{originData.count}}  {{originData.user.name}} </p>
+<button @click="increaseCount">click me to increase the count</button>
 </template>
 
 
 <script setup lang="ts">
+import {ref,reactive} from "vue"
+
+// const count = ref(0);
+// const user = reactive({name:'san Zhang'});
+// const arr = reactive([1,2,3,4])
+
+// zong he dingyi
+const originData = reactive({
+    count:0,
+    user:{
+        name:'san zhang'
+    },
+    arr:[1,2,3,4]
+})
+const increaseCount = ()=>{
+   originData.count++;
+   originData.user.name = 'si Li';
+}
 const props = defineProps({
     msg:{
         type:String,
         default:()=>'default value'
+
     }
    
 })
